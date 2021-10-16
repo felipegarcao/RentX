221332a643c11ee78d4089a14c167dd05e78dd38
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-10-16 09:34:29 -0300

    :fire: Trabalhando com Upload

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,16 +1,13 @@
-import { Request, Response} from 'express'
-import { ListCategoriesUseCase } from './ListCategoriesUseCase';
-
+import { Request, Response } from "express";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
   constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
   handle(request: Request, response: Response): Response {
-    const all = this.listCategoriesUseCase.execute()
+    const all = this.listCategoriesUseCase.execute();
 
     return response.json(all);
-
   }
-
 }
 
 export { ListCategoriesController };
